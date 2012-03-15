2936a6f32ca7d960e3e2d4e55d2e3433b6ca94dd
Daniel Parnell <d@djlp.net>
2012-03-15 21:47:06 +1100

    Move the ears randomly

--- p4.php
+++ p4.php
@@ -9,7 +9,7 @@ array_push($data, 0x03, 0x00, 0x00, 0x01, 10);
 # build up an ambient block
 $ambient = array();
 encode_set_ambient($ambient, $WEATHER, $SUNNY);
-encode_ear_positions($ambient, 5, 0);
+encode_ear_positions($ambient, rand(0, 18), rand(0, 18));
 #encode_clear_ambient($ambient, $WEATHER);
 array_push($data, 4);
 encode_length($data, count($ambient) + 4);
