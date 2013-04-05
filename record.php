4e82011a37d40c9de34bdbd01abf47551f86b5ed
Daniel Parnell <me@danielparnell.com>
2013-04-05 11:52:14 +1100

    Better error handling

--- record.php
+++ record.php
@@ -6,60 +6,62 @@ $temp_file = tempnam(sys_get_temp_dir(), 'nabaztag');
 file_put_contents($temp_file.".wav", $HTTP_RAW_POST_DATA);
 exec("ffmpeg -i ".$temp_file.".wav -ar 16000 -y ".$temp_file.".flac");
 
-  if(isset($config)) {
-    $rabbit = find_rabbit($db, $_REQUEST['sn']);
-    
-    $url = "http://www.google.com/speech-api/v1/recognize?xjerr=1&client=nabaztag-php&lang=en-US&maxresults=10";
-    $params = array('http' => array(
-				    'method' => 'POST',
-				    'header' => "Content-Type: audio/x-flac; rate=16000\r\n",
-				    'content' => file_get_contents($temp_file.".flac")
-				    ));
-    $ctx = stream_context_create($params);
-    $fp = @fopen($url, 'rb', false, $ctx);
-    if (!$fp) {
-      $error = "Could not send data for recognition: ".$php_errormsg;
+if(isset($config)) {
+  $rabbit = find_rabbit($db, $_REQUEST['sn']);
+
+  $url = "http://www.google.com/speech-api/v1/recognize?xjerr=1&client=nabaztag-php&lang=en-US&maxresults=10";
+  $params = array('http' => array(
+                                  'method' => 'POST',
+                                  'header' => "Content-Type: audio/x-flac; rate=16000\r\n",
+                                  'content' => file_get_contents($temp_file.".flac")
+                                  ));
+  $ctx = stream_context_create($params);
+  $fp = @fopen($url, 'rb', false, $ctx);
+  if (!$fp) {
+    $error = "Could not send data for recognition: ".$php_errormsg;
+  } else {
+    $response = @stream_get_contents($fp);
+
+#    error_log($response);
+
+    if ($response === false) {
+      $error = "Could not read response: ".$php_errormsg;
     } else {
-      $response = @stream_get_contents($fp);
+      $error = null;
 
-      if ($response === false) {
-	$error = "Could not read response: ".$php_errormsg;
-      } else {
-	$error = null;
+      $data = json_decode($response);
+      $hypotheses = $data->{'hypotheses'};
+      $utterance = $hypotheses[0]->{'utterance'};
+      $words = explode(' ', $utterance);
+      $found = false;
+      foreach($words as $word) {
+        try {
+          $to_load = 'apps/'.$utterance.'_command.php';
+          if(file_exists($APP_DIR.'/'.$to_load)) {
+            $found = true;
+            include($to_load);
 
-	$data = json_decode($response);
-	$hypotheses = $data->{'hypotheses'};
-	$utterance = $hypotheses[0]->{'utterance'};
-	$words = explode(' ', $utterance);
-	$found = false;
-	foreach($words as $word) {
-	  try {
-	    $to_load = 'apps/'.$utterance.'_command.php';
-	    if(file_exists($APP_DIR.'/'.$to_load)) {
-	      $found = true;
-	      include($to_load);
-	  
-	      call_user_func($utterance."_command", $db, $rabbit);
-	    }
-	  } catch (Exception $e) {
-	    $error = "Error processing command: ".$e->getMessage();
-	  }
-	}
-	
-	if(!$found) {
-	  $error = "I don't know how to: ".$utterance;
-	}
+            call_user_func($utterance."_command", $db, $rabbit);
+          }
+        } catch (Exception $e) {
+          $error = "Error processing command: ".$e->getMessage();
+        }
+      }
+
+      if(!$found) {
+        $error = "I don't know how to: ".$utterance;
       }
     }
-  } 
-
-  if($error) {
-    $app = array('application' => 'tts', 'data' => serialize(array('text' => $error)));
-    save_rabbit_app($db, $rabbit, $app);
   }
+}
 
-  unlink($temp_file);
-  unlink($temp_file.".wav");
-  unlink($temp_file.".flac");
+if($error) {
+  $app = array('application' => 'tts', 'data' => serialize(array('text' => $error)));
+  save_rabbit_app($db, $rabbit, $app);
+}
+
+unlink($temp_file);
+unlink($temp_file.".wav");
+unlink($temp_file.".flac");
 
 ?>
