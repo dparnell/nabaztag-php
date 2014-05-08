fb33562c6dc9223c73f03a7cbd8289478181e62f
Daniel Parnell <me@danielparnell.com>
2014-05-09 09:16:39 +1000

    Fixed speech recognition under Ubuntu 14.04

--- record.php
+++ record.php
@@ -5,24 +5,33 @@ require('lib/rabbit.php');
 $temp_file = tempnam(sys_get_temp_dir(), 'nabaztag');
 file_put_contents($temp_file.".wav", $HTTP_RAW_POST_DATA);
 exec("ffmpeg -i ".$temp_file.".wav -ar 16000 -y ".$temp_file.".flac");
+if(!file_exists($temp_file.".flac")) {
+  # try avconv instead
+  exec("avconv -i ".$temp_file.".wav -ar 16000 -y ".$temp_file.".flac");
+}
 
 if(isset($config)) {
   $rabbit = find_rabbit($db, $_REQUEST['sn']);
 
   $url = "http://www.google.com/speech-api/v1/recognize?xjerr=1&client=nabaztag-php&lang=en-US&maxresults=10";
-  $params = array('http' => array(
-                                  'method' => 'POST',
-                                  'header' => "Content-Type: audio/x-flac; rate=16000\r\n",
-                                  'content' => file_get_contents($temp_file.".flac")
-                                  ));
-  $ctx = stream_context_create($params);
-  $fp = @fopen($url, 'rb', false, $ctx);
-  if (!$fp) {
+  $flac_file = file_get_contents($temp_file.".flac");
+  $flac_size = filesize($temp_file.".flac");
+
+  $curl = curl_init();
+  curl_setopt_array($curl, array(
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_URL => $url,
+    CURLOPT_HTTPHEADER => array("Content-Type: audio/x-flac; rate=16000", "Content-Length: $flac_size", "Expect:"),
+    CURLOPT_POST => true,
+    CURLOPT_POSTFIELDS => $flac_file
+  ));
+  $response = curl_exec($curl);
+  curl_close($curl);
+
+  if (!$response) {
     $error = "Could not send data for recognition: ".$php_errormsg;
   } else {
-    $response = @stream_get_contents($fp);
-
-#    error_log($response);
+    error_log($response);
 
     if ($response === false) {
       $error = "Could not read response: ".$php_errormsg;
