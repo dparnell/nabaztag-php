4dce948905be7888f5bf4c1e1a831264bebf6fb8
Daniel Parnell <me@danielparnell.com>
2014-10-05 17:16:30 +1100

    Fixed recurrence when setting it to "Never"

--- apps/recurrence.php
+++ apps/recurrence.php
@@ -3,7 +3,7 @@
   <input type="text" name="next_update" id="next_update" value="<?php echo app_next_update_time($app); ?>"/>
   <label for="next_update">Recurring:</label>
   <select name="interval">
-    <option>Never</option>
+    <option value="0">Never</option>
     <?php
     $intervals = array("60" => "Every minute", "300" => "Every 5 minutes", "900" => "Every 15 minutes", "1800" => "Every 30 minutes", "3600" => "Every hour", "43200" => "Evert 12 hours", "86400" => "Every day", "604800" => "Every week");
     foreach($intervals as $key => $value) {
